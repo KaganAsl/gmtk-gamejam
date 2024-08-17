b3407ca3274dea22413f00d1748c48b33dd05658
KaganAsl <kagan.aslancan@gmail.com>
2024-08-17 23:48:09 +0300

    Timer added to food

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodSpawnerScript : MonoBehaviour
+public class FoodScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
